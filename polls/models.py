9e452fa5e00adaa8570dba85494394858e1f6a6c
Paul Ok <pso3td@virginia.edu>
2020-09-16 18:52:29 -0400

    sprint1

--- polls/models.py
+++ polls/models.py
@@ -1,20 +1,16 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-<<<<<<< HEAD
-=======
 
->>>>>>> Paul
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-<<<<<<< HEAD
     votes = models.IntegerField(default=0)
-=======
-    votes = models.IntegerField(default=0)
->>>>>>> Paul
--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-<<<<<<< HEAD
-# Create your views here.
-
-def index(request):
-    return HttpResponse("Hello, world.  You're at the polls index.")
-=======
 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
->>>>>>> Paul
