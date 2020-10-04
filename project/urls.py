794d173ab683dc3586a69f7d2f3d426a4465ebde
Paul Ok <pso3td@virginia.edu>
2020-10-04 17:11:54 -0400

    Google Login Integration

--- project/urls.py
+++ project/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('', TemplateView.as_view(template_name="donation_app/index.html")),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
 ]
