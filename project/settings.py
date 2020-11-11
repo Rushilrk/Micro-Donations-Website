a639f57556dca1995fa685d92ca5fc7291205179
Paul Ok <pso3td@virginia.edu>
2020-11-11 12:09:35 -0500

    small changes

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'u=y*+cn7y-if_v_23!+0trup&h9cihu@qodsc3*%7dkkpph_bi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
