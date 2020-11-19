08a228be9cdfd6d7fb7046aff67ef9a1a6663972
Paul Ok <pso3td@virginia.edu>
2020-11-19 01:12:38 -0500

    postgres heroku? 2

--- project/settings.py
+++ project/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'donationapp',
-        'USER': 'paul',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
