20d647313ee8f1defb1f8553f82ad24582005f78
gjs4wu <gjs4wu@virginia.edu>
2020-11-19 01:13:36 -0500

    postgresql

--- project/settings.py
+++ project/settings.py
@@ -94,6 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'travis_ci_test',
         'USER': 'postgres',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
